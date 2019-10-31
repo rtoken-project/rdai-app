4a9a8576173cfcb73c27a75da7e6d3688482ab8a
Miao ZhiCheng <miao@decentral.ee>
2019-10-31 21:46:55 +0200

    increase gasMultiplier to 1.5

--- front/app/src/store/contracts.js
+++ front/app/src/store/contracts.js
@@ -1,33 +1,35 @@
 import * as sdk from "@decentral.ee/rtoken-contracts";
 
 const contracts = {
-    IRToken: null,
-    IERC20: null,
-    tokens: null,
-    functions: null,
-    faucet: null,
+  IRToken: null,
+  IERC20: null,
+  tokens: null,
+  functions: null,
+  faucet: null,
 
-    init: async function(web3, allTokens, chainId) {
-        const tokens = allTokens[chainId];
-        const contracts = sdk.load(web3.currentProvider);
-        this.IRToken = contracts.IRToken;
-        this.IERC20 = contracts.IERC20;
-        this.functions = await this.IRToken.at(tokens.rdai);
-        if (chainId == 4) {
-            this.faucet = await contracts.RinkebyTestDAI.at(
-                "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"
-            );
-        }
-        // load all tokens and await all to be done
-        var tokensTemp = {};
-        await Promise.all(
-            Object.keys(tokens).map(async token => {
-                console.log(`Loading token ${token} at ${tokens[token]}`);
-                tokensTemp[token] = await this.IERC20.at(tokens[token]);
-            })
-        );
-        this.tokens = tokensTemp;
+  init: async function(web3, allTokens, chainId) {
+    const tokens = allTokens[chainId];
+    const contracts = sdk.load(web3.currentProvider);
+    this.IRToken = contracts.IRToken;
+    // increase gasMultiplier in case compound may require more gas
+    this.IRToken.gasMultiplier = 1.5;
+    this.IERC20 = contracts.IERC20;
+    this.functions = await this.IRToken.at(tokens.rdai);
+    if (chainId == 4) {
+      this.faucet = await contracts.RinkebyTestDAI.at(
+        "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"
+      );
     }
+    // load all tokens and await all to be done
+    var tokensTemp = {};
+    await Promise.all(
+      Object.keys(tokens).map(async token => {
+        console.log(`Loading token ${token} at ${tokens[token]}`);
+        tokensTemp[token] = await this.IERC20.at(tokens[token]);
+      })
+    );
+    this.tokens = tokensTemp;
+  }
 };
 
 export default contracts;
